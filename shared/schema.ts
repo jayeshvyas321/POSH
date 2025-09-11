f70f46b061d9216f96466a7bae99da638a4d7a8b
jayeshvyas321 <jayesh.vyas@zucitech.com>
2025-09-11 12:22:07 +0530

    changes for login

--- shared/schema.ts
+++ shared/schema.ts
@@ -38,7 +38,7 @@ export const insertNotificationSchema = createInsertSchema(notifications).omit({
 
 // Login schema
 export const loginSchema = z.object({
-  email: z.string().email("Please enter a valid email address"),
+  userNameOrEmail: z.string().email("Please enter a valid email address"),
   password: z.string().min(6, "Password must be at least 6 characters"),
 });
 
